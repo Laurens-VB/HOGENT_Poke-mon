ecbb6541cc6cee734a0785348c3e699c8cd946ef
Laurens Van Bruwaene <laurens.vanbruwaene@gmail.com>
2022-01-03 04:02:05 +0100

    Initial Commit

---
+++ HOGENT_Pokémon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HOGENT_Pokémon
+//
+//  Created by LaurensVB on 03/01/2022.
+//  Copyright © 2022 LaurensVB. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
